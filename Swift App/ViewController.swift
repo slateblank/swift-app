5a1b24b72dbc7d8f2f5e9ddbbbe5d5dce159a7bb
Matt Harp <mattharp@Matts-MacBook-Pro.local>
2017-02-04 15:38:46 -0600

    Added dot after All Rights Reserved

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -3,16 +3,36 @@
 //  Swift App
 //
 //  Created by Matt Harp on 2/2/17.
-//  Copyright © 2017 Matt Harp. All rights reserved.
+//  Copyright © 2017 Matt Harp. All rights reserved..
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    //Changes hello world to matt on click
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = "Matt"
+        //counts how many ties button has been tapped
+        tapCount += 1
+        if tapCount >= 10 {
+             theLabel.text = "You tapped the button 10 times"
+        } else {
+             print(tapCount)
+        }
+       
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
